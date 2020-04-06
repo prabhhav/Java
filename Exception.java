086d55b756b0d8caf47470bf0ecce6d624056618
Prabhhav Sharma <prabhhavsharma@karunya.edu.in>
2020-04-06 18:55:55 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,22 @@
+
+package exception;
+
+public class Exception {
+
+   
+    public static void main(String[] args) {
+        
+        try{
+            // code that may raise exception 
+            int data = 100/0;
+            
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("e");
+        }
+        System.out.println("rest of the code...");
+        
+    }
+    
+}
